db30eb61c0bdbe6be07a41737cd62c7c44c89db3
MatthewWeisCaps <Matthewweiscaps@gmail.com>
2018-02-09 21:59:12 -0600

    PlatformBuilder
    added default platform builder

---
+++ core/src/com/jam/game/b2d/Box2dPlatformBuilder.java
@@ -0,0 +1,153 @@
+package com.jam.game.b2d;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Filter;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Disposable;
+
+/*
+ * Builder for platforms.
+ * Currently supports only 1 shape/fixture per body.
+ */
+public class Box2dPlatformBuilder implements Disposable {
+	
+	private Body b2dBody;
+	private BodyDef b2dBodyDef;
+	private Shape b2dShape;
+	private FixtureDef b2dFixture;
+	
+	/*
+	 * Returns the builder for chaining.
+	 */
+	public Box2dPlatformBuilder setBodyType(BodyType type) {
+		b2dBodyDef.type = type;
+		return this;
+	}
+	
+	/*
+	 * Returns the builder for chaining.
+	 */
+	public Box2dPlatformBuilder setBodyPosition(Vector2 position) {
+		b2dBodyDef.position.set(position);
+		return this;
+	}
+	
+	/*
+	 * Returns the builder for chaining.
+	 */
+	public Box2dPlatformBuilder setBodyPosition(float x, float y) {
+		b2dBodyDef.position.set(x, y);
+		return this;
+	}
+	
+	/*
+	 * Returns the builder for chaining.
+	 */
+	public Box2dPlatformBuilder setShape(Shape shape) {
+		b2dShape = shape;
+		b2dFixture.shape = b2dShape;
+		return this;
+	}
+	
+	/*
+	 * Returns the builder for chaining.
+	 */
+	public Box2dPlatformBuilder setDensity(float density) {
+		b2dFixture.density = density;
+		return this;
+	}
+	
+	/*
+	 * Returns the builder for chaining.
+	 */
+	public Box2dPlatformBuilder setFilter(Filter filter) {
+		b2dFixture.filter.categoryBits = filter.categoryBits;
+		b2dFixture.filter.maskBits = filter.maskBits;
+		b2dFixture.filter.groupIndex = filter.groupIndex;
+		return this;
+	}
+	
+	/*
+	 * Returns the builder for chaining.
+	 */
+	public Box2dPlatformBuilder setFriction(float friction) {
+		b2dFixture.friction = friction;
+		return this;
+	}
+	
+	/*
+	 * Returns the builder for chaining.
+	 */
+	public Box2dPlatformBuilder setRestitution(float restitution) {
+		b2dFixture.restitution = restitution;
+		return this;
+	}
+	
+	/*
+	 * Returns the builder for chaining.
+	 */
+	public Box2dPlatformBuilder isSensor(boolean isSensor) {
+		b2dFixture.isSensor = isSensor;
+		return this;
+	}
+	
+	/*
+	 * Build method. Disposes of shape, not body.
+	 */
+	public Body buildAndDispose(World world) {
+		Body body = this.build(world);
+		this.dispose();
+		return body;
+	}
+	
+	/*
+	 * Clear all options.
+	 */
+	public Box2dPlatformBuilder clear() {
+		this.dispose();
+		return new Box2dPlatformBuilder();
+	}
+	
+	/*
+	 * Build method. Does not dispose of shape.
+	 */
+	public Body build(World world) {
+		b2dBody = world.createBody(b2dBodyDef);
+		b2dBody.createFixture(b2dFixture);
+		return b2dBody;
+	}
+	
+	public Box2dPlatformBuilder() {
+		b2dBodyDef = new BodyDef();
+		b2dFixture = new FixtureDef();
+	}
+	
+	public static Box2dPlatformBuilder DEFAULT() {
+		
+		Box2dPlatformBuilder builder = new Box2dPlatformBuilder();
+		
+		builder.setBodyType(BodyType.StaticBody);
+		builder.setBodyPosition(0, 0);
+		builder.setDensity(0.8f);
+		builder.setFriction(0.08f);
+		builder.setRestitution(0.9f);
+		
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(1.5f, 0.5f);
+		builder.setShape(shape);
+	
+		return builder;
+	}
+
+	@Override
+	public void dispose() {
+		b2dShape.dispose();
+	}
+	
+}
