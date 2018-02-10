8ba0f1fa4290dbf14aa5dafe474e8f1319f52d36
Ryan Kruse <rpkruse@ksu.edu>
2018-02-09 21:51:32 -0600

    Added temp player movement

--- core/src/com/jam/game/levels/LevelFactory.java
+++ core/src/com/jam/game/levels/LevelFactory.java
@@ -2,4 +2,7 @@ package com.jam.game.levels;
 
 public class LevelFactory {
 	
+	public LevelFactory() {
+		
+	}
 }
--- core/src/com/jam/game/screens/GameScreen.java
+++ core/src/com/jam/game/screens/GameScreen.java
@@ -1,7 +1,14 @@
 package com.jam.game.screens;
 
+import java.awt.Rectangle;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g3d.particles.values.RectangleSpawnShapeValue;
+import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.BoxShapeBuilder;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -9,19 +16,22 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 
 public class GameScreen implements Screen {
 	
-	public static final int VIRTUAL_WIDTH = 480;
-	public static final int VIRTUAL_HEIGHT = 320;
-	public static final int UNIT = 32;
+	public static final int VIRTUAL_WIDTH = 480/8;//480
+	public static final int VIRTUAL_HEIGHT = 320/8; //320
+	public static final int UNIT = 2;
 	
 	World b2dWorld;
 	Box2DDebugRenderer b2dRenderer;
 	OrthographicCamera camera;
 	
+	Body player;
+	int speed = 50;
 	
 	@Override
 	public void show() {
@@ -31,29 +41,51 @@ public class GameScreen implements Screen {
 //		camera.lookAt(100.0f, 100.0f, 0.0f);
 		camera.position.set(new Vector3(100.0f, 100.0f, 0.0f));
 		
+		//Temp Player
+		BodyDef playerBodyDef = new BodyDef();
+		playerBodyDef.type = BodyDef.BodyType.DynamicBody;
+		playerBodyDef.position.set(0, 0);
+		
+		player = b2dWorld.createBody(playerBodyDef);
+		
+		FixtureDef playerFixture = new FixtureDef();
+		
+		PolygonShape boxShape = new PolygonShape();
+		boxShape.setAsBox(UNIT/2, UNIT/2);
+		
+		playerFixture.shape = boxShape;
+		playerFixture.restitution = 0.4f;
+		
+		player.createFixture(playerFixture);
+		
+		boxShape.dispose();
+		
 		// test
 		BodyDef b2dBodyDef = new BodyDef();
 		b2dBodyDef.type = BodyDef.BodyType.StaticBody;
-		b2dBodyDef.position.set(100, 100);
+		b2dBodyDef.position.set(0, -VIRTUAL_HEIGHT/2);
 		
 		Body b2dBody = b2dWorld.createBody(b2dBodyDef);
 		
-		FixtureDef b2dFixture = new FixtureDef();
+		FixtureDef floorFixture = new FixtureDef();
 		
-		CircleShape circle = new CircleShape();
-		circle.setRadius(6.0f);
-		b2dFixture.shape = new CircleShape();
-		b2dFixture.density = 0.5f;
+		PolygonShape floor = new PolygonShape();
+		floor.setAsBox(VIRTUAL_WIDTH, UNIT/2);
 		
-		b2dBody.createFixture(b2dFixture);
+		floorFixture.shape = floor;
+		floorFixture.restitution = 0.4f;
 		
-		circle.dispose();
+		b2dBody.createFixture(floorFixture);
+		floor.dispose();
+		
+		setupInputHandler();
 		
 	}
 
 	@Override
 	public void render(float delta) {
 		// logic
+		checkInput();
 		
 		b2dWorld.step(delta, 8, 3);
 		
@@ -88,6 +120,86 @@ public class GameScreen implements Screen {
 	public void dispose() {
 		b2dWorld.dispose();
 		b2dRenderer.dispose();
+	}
+	
+	void checkInput() {
+		float xDir = 0;
+		float yDir = 0;
+		
+		if(Gdx.input.isKeyPressed(Keys.W)) {
+			yDir = 1;
+		} 
+		if(Gdx.input.isKeyPressed(Keys.S)) {
+			yDir = -1;
+		}
+		
+		if(Gdx.input.isKeyPressed(Keys.A)) {
+			xDir = -1;
+		} 
+		if(Gdx.input.isKeyPressed(Keys.D)) {
+			xDir = 1;
+		}
+		
+		
+		player.setLinearVelocity(new Vector2(xDir * speed, yDir * speed));
+		
+		
+	}
+	
+	void setupInputHandler() {
+		Gdx.input.setInputProcessor(new InputProcessor() {
+
+			
+			@Override
+			public boolean keyDown(int keycode) {
+				switch(keycode) {
+					case Keys.ESCAPE:
+						Gdx.app.exit();
+				}
+				return false;
+			}
+
+			@Override
+			public boolean keyUp(int keycode) {
+				return false;
+			}
+
+			@Override
+			public boolean keyTyped(char character) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDragged(int screenX, int screenY, int pointer) {
+				return false;
+			}
+
+			@Override
+			public boolean mouseMoved(int screenX, int screenY) {
+				return false;
+			}
+
+			@Override
+			public boolean scrolled(int amount) {
+				camera.zoom += 0.05 * amount;
+				
+//				camera.position.set(camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0.0f)));
+				
+				camera.update();
+				return false;
+			}
+			
+		});
 	}
 
 }
